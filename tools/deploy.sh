04026b4cbbc3019e7ca4a6d7a11e6c3e967e832d
shanej90 <shane.jackson@gmx.co.uk>
2024-10-07 09:21:14 +0100

    tweaked deployment script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,5 @@
-$ pelican content -o output -s pelicanconf.py
-$ ghp-import output -b gh-pages
-$ git push origin gh-pages
+#!/bin/bash
+
+pelican content -o output -s pelicanconf.py
+ghp-import output -b gh-pages
+git push origin gh-pages
